4d7c45abbb33ea7866141fbcc7213e339336c098
Kayla <holloway_kayla@columbusstate.edu>
2019-11-05 21:08:24 -0500

    Added Weapons and Enemies

---
+++ StarterGame/Machete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychWard
+{
+    class Machete: I_Item, IWeapon
+    {
+        private String name;
+        public String Name { get; }
+        private float weight;
+        public float Weight { get; }
+        private String description;
+        public String Description { get; }
+        private Boolean keyItem;
+        public Boolean KeyItem { get; }
+        private int value;
+        public int Value { get; }
+
+        private int uses;
+        public int Uses { get; }
+
+        public void useItem()
+        {
+            //work on. player>item>useItem()>COMMAND
+        }
+        public int Attack { get; }
+
+        private int strength;
+        public int Strength { get; }
+        public void getStrength(Player player)
+        {
+           
+        }
+
+
+    }
+}
---
+++ StarterGame/ZombiePatient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychWard
+{
+    class ZombiePatient :IEnemy
+    {
+        public int Level { get; }
+        private int attack;
+        public int Attack { get; }
+        private int defense;
+        public int Defense { get; }
+        private float hitprobability;
+        public float HitProbability { get; }
+        public String AttackDescription { get; }
+    }
+}
