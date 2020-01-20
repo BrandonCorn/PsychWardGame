b9d134dcf6e1001898adb227cda5376da28d3724
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-01-20 13:51:52 -0500

    adding comments

--- StarterGame/GameWorld/Merchant/Merchant.cs
+++ StarterGame/GameWorld/Merchant/Merchant.cs
@@ -47,13 +47,22 @@ namespace StarterGame
 
         }
 
-        //add tasks to the merchants list
+        /**
+         * Add tasks to the merchants list
+         * @params: (ITask)The task to be added to the merchant's list. 
+         * @returns: void
+         **/
+
         public void addTask(ITask task)
         {
             this.taskList.Enqueue(task);
         } 
 
-        //When the player enters the merchant room, the commands allowed in the merchant room are set. 
+        /**When the player enters the merchant room, the commands allowed in the merchant room are set. 
+         * @params: (Notification) notification that is sent from the Game World letting the merchant know the player has 
+         * entered the room
+         * @returns: void
+         **/
         private void PlayerSpeak_merchant(Notification notification)
         {
             Player player = (Player)notification.Object;
@@ -65,14 +74,21 @@ namespace StarterGame
             Console.WriteLine("\nWould you like to: \n\tbuy goods" + "\n\tsell goods" + "\n\trepair weapon");
         }
         
-        //Displays when interaction with merchent ends. 
+        /**Displays when interaction with merchent ends. 
+         * @params: (Notification) notification from player's walk away command that ends the let's the merchant know the 
+         * interaction with the player is over. 
+         * @returns: void
+         **/
         public void LeaveMerchant(Notification notification)
         {
             Console.WriteLine("\n\nThank's for your business. Come again soon!\n\n");
             
         }
 
-        //Gives the new player a backpack. 
+        /**Gives the new player a backpack. 
+         * @params: The player who the merchant is giving the key item backpack. 
+         * @returns: void
+         **/
         public void giveBackpack(Player player)
         {
             if (player.Backpack == null)
@@ -85,6 +101,11 @@ namespace StarterGame
             }
         }
 
+        /**
+         * Gives the player a task on the condition that they have completed their current task or do not have one currently set. 
+         * @params: (Player) The player who is being given the task. 
+         * @returns: void
+         **/
         public void givePlayerTask(Player player)
         {
             if (player.CurrentTask == null || player.CurrentTask.TaskState == TaskState.Complete)
@@ -101,6 +122,12 @@ namespace StarterGame
             }
         }
 
+        /**
+         * Displays the inventory the merchant has for sale. 
+         * @params: (Notification) notification from the player's merchandise command that they would like to view the inventory
+         * the merchant has for sale. 
+         * @return: void
+         **/
         public void ViewInventory(Notification notification)
         {
             string itemDisplay = "";
@@ -116,6 +143,11 @@ namespace StarterGame
             Console.WriteLine("****************************************************\n");
         }
 
+        /**
+         * Retrieves an item from the merchant's inventory that they have available for purchase. 
+         * @params: (String) The name of the item to be retrieved. 
+         * @returns: (I_Item) The item that is in the inventory if available. 
+         **/
         public I_Item itemFromInventory(string name)
         {
             I_Item item = null;
@@ -123,11 +155,22 @@ namespace StarterGame
             return item;
         }
 
+        /**
+         * Message thanking the player for purchasing an item. 
+         * @params: (Notification) notification from the player's buy command that they have purchased an item. 
+         * @returns: void 
+         **/
         public void PlayerPurchaseMessage(Notification notification)
         {
             Console.WriteLine("Thank you for your purchase!");
         }
 
+        /**
+         * Let's the merchant know that they player has purchased the repair of a weapon and the merchant repairs it. 
+         * @params: (Notification) notification from the player's repair command that they have purchased a weapon repair. The
+         * notification object provides the weapon to be repaired. 
+         * @returns: void
+         **/
         public void RepairWeapon(Notification notification)
         {
             IWeapon weapon = (IWeapon)notification.Object;
