81c197ca53eab6a4ff544d53e754489340021b3f
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-01-20 13:55:18 -0500

    adding comments

--- StarterGame/Items/IStatItem.cs
+++ StarterGame/Items/IStatItem.cs
@@ -6,9 +6,16 @@ namespace StarterGame
 {
     public interface IStatItem
     {
+        //Attack increase of the stat boost item
         int Attack {get;}
+
+        //Speed increase of the stat boost item.
         int Speed { get; }
+
+        //Block increase of the stat boost item. 
         int Block { get; }
+
+        //Number of uses of the stat boost item. 
         int Uses { get; set; }
     }
 }
